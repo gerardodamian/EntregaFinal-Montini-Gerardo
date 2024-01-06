a093d99ba48caef64435f382962c4cfce813b5dd
Gerardo Montini <germontini@hotmail.com>
2024-01-06 15:29:57 -0300

    articulos

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,15 +1,13 @@
-import CarritoCom from "../assets/cart4.svg"
+import CarritoCom from "../assets/cart4.svg";
 
 const CartWidget = () => {
     return (
         <button type="button" className="btn btn-primary position-relative">
-  <img src={CarritoCom} alt="carrito" width={40} />
-  <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-    4
-    <span className="visually-hidden">unread messages</span>
-  </span>
-</button>
-      
+            <img src={CarritoCom} alt="carrito" width={40} />
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                4<span className="visually-hidden">unread messages</span>
+            </span>
+        </button>
     );
 };
 export default CartWidget;
